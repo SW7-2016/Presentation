ad0117e48e2c6e8fe551d80e3516cf939352e2f8
Software7 <sw7@h57.net.klient.slv.site.aau.dk>
2016-11-15 11:28:32 +0100

    Removed comment

--- website/config.py
+++ website/config.py
@@ -7,7 +7,7 @@ class BaseConfig(object):
     HOST = '0.0.0.0'
     PORT = 9000
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Kaffe23!@localhost/tester'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://USER:PASSWORD@localhost/tester'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
